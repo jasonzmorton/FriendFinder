88f140c1a6643083def03664c29e9af79fd94907
jason Morton <jasonzmorton@knights.ucf.edu>
2018-05-03 17:29:18 -0400

    structur

---
+++ app/data/friends.js
@@ -0,0 +1,16 @@
+var friends = [
+    {
+        name: "Tony",
+        age: 60
+    },
+    {
+        name: "sarah",
+        age: 18
+    },
+    {
+        name: "Ahmed",
+        age: 45
+    },
+
+]
+module.exports = friends
